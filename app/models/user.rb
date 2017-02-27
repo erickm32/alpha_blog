411890c64c8cc6b680275718650f9ed8880e3e3d
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2017-02-27 10:58:53 -0300

    Signup users from UI.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  has_secure_password
   has_many :articles
   before_save { self.email = email.downcase }
   validates :username, presence: true,
@@ -11,4 +10,6 @@ class User < ActiveRecord::Base
             length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+            
+  has_secure_password
 end
